cd467f35d745123870b50b8dd03a15d8644534e9
Amelia Fordyce <49503151+afordyce564@users.noreply.github.com>
2022-12-22 12:18:01 -0500

    Update code_example.go

--- PLP/plp2/code_example.go
+++ PLP/plp2/code_example.go
@@ -11,4 +11,20 @@ func main() {
 	var y = 6
 
 	fmt.Println(x + y)
+	
+	//you cannot make an array with multiple data types
+	// you have to define the type
+	var groceryList = []string{"hello", "world", "is", "good"}
+	
+	//Integers can be converted to other types of Integers, Floats, and Strings
+	var litte int8
+	
+	big = int64(little)
+	
+	//Floats can be converted into Integers and Strings
+	//For Floats to Ints, use the int() function
+	int(4.0)
+	
+	//To convert Integers and Floats into Strings, you must import the strconv package and use the strconv.Itoa function
+	twelve = strconv.Itoa(12)
 }
